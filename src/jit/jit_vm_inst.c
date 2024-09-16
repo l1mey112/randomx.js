d4675d4251e4ce09dcc30b6fd522829bff9a88c8
l1mey112 <l-m@l-m.dev>
2024-09-16 19:42:45 +1000

    vm+jit: floating point instructions

--- src/jit/jit_vm_inst.c
+++ src/jit/jit_vm_inst.c
@@ -49,6 +49,8 @@ OPCODE_CEIL_DECLARE(NOP, ISTORE);
 #define SCRATCHPAD_L2_MASK ((RANDOMX_SCRATCHPAD_L2 / 8 - 1) * 8)
 #define SCRATCHPAD_L3_MASK ((RANDOMX_SCRATCHPAD_L3 / 8 - 1) * 8)
 
+// i64.const $imm64
+// i64.add
 // i32.wrap_i64
 // i32.const $mask (L1 or L2)
 // i32.and
@@ -56,23 +58,68 @@ OPCODE_CEIL_DECLARE(NOP, ISTORE);
 // i32.add
 // i64.load align=2
 #define SCRATCHPAD_LOAD_L1_L2(inst)                                         \
-	WASM_U8_THUNK({0xa7, 0x41});                                            \
+	WASM_U8_THUNK({0x42});                                                  \
+	WASM_I64(IMM_SEXT64(inst->imm32));                                      \
+	WASM_U8_THUNK({0x7c, 0xa7, 0x41});                                      \
 	WASM_I64(MOD_MEM(inst->mod) ? SCRATCHPAD_L1_MASK : SCRATCHPAD_L2_MASK); \
 	WASM_U8_THUNK({0x71, 0x41});                                            \
 	WASM_I64((int64_t)scratchpad);                                          \
 	WASM_U8_THUNK({0x6a, 0x29, 2, 0});
 
+// i64.const $imm64
+// i64.add
+// i32.wrap_i64
+// i32.const $mask (L1 or L2)
+// i32.and
+// i32.const $scratchpad
+// i32.add
+// v128.load64_zero align=3 offset=0
+// f64x2.convert_low_i32x4_s
+#define SCRATCHPAD_LOAD_F_L1_L2(inst)                                       \
+	WASM_U8_THUNK({0x42});                                                  \
+	WASM_I64(IMM_SEXT64(inst->imm32));                                      \
+	WASM_U8_THUNK({0x7c, 0xa7, 0x41});                                      \
+	WASM_I64(MOD_MEM(inst->mod) ? SCRATCHPAD_L1_MASK : SCRATCHPAD_L2_MASK); \
+	WASM_U8_THUNK({0x71, 0x41});                                            \
+	WASM_I64((int64_t)scratchpad);                                          \
+	WASM_U8_THUNK({0x6a, 0xfd, 0x5d, 3, 0, 0xfd, 0xfe, 0x01});
+
+// i64.const $imm64
+// i64.add
+// i32.wrap_i64
+// i32.const $mask (L1 or L2)
+// i32.and
+// i32.const $scratchpad
+// i32.add
+// v128.load64_zero align=3 offset=0
+// f64x2.convert_low_i32x4_s
+// local.get $mask_mant
+// v128.and
+// local.get $mask_exp
+// v128.or
+#define SCRATCHPAD_LOAD_E_L1_L2(inst)                                       \
+	WASM_U8_THUNK({0x42});                                                  \
+	WASM_I64(IMM_SEXT64(inst->imm32));                                      \
+	WASM_U8_THUNK({0x7c, 0xa7, 0x41});                                      \
+	WASM_I64(MOD_MEM(inst->mod) ? SCRATCHPAD_L1_MASK : SCRATCHPAD_L2_MASK); \
+	WASM_U8_THUNK({0x71, 0x41});                                            \
+	WASM_I64((int64_t)scratchpad);                                          \
+	WASM_U8_THUNK({0x6a, 0xfd, 0x5d, 3, 0, 0xfd, 0xfe, 0x01, 0x20, $mask_mant, 0xfd, 0x4e, 0x20, $mask_exp, 0xfd, 0x50});
+
+// i64.const $imm64
 // i32.wrap_i64
 // i32.const $mask (L3)
 // i32.and
 // i32.const $scratchpad
 // i32.add
 // i64.load align=2
-#define SCRATCHPAD_LOAD_L3(inst)   \
-	WASM_U8_THUNK({0xa7, 0x41});   \
-	WASM_I64(SCRATCHPAD_L3_MASK);  \
-	WASM_U8_THUNK({0x71, 0x41});   \
-	WASM_I64((int64_t)scratchpad); \
+#define SCRATCHPAD_LOAD_L3(inst)       \
+	WASM_U8_THUNK({0x42});             \
+	WASM_I64(IMM_SEXT64(inst->imm32)); \
+	WASM_U8_THUNK({0xa7, 0x41});       \
+	WASM_I64(SCRATCHPAD_L3_MASK);      \
+	WASM_U8_THUNK({0x71, 0x41});       \
+	WASM_I64((int64_t)scratchpad);     \
 	WASM_U8_THUNK({0x6a, 0x29, 2, 0});
 
 uint32_t jit_vm_inst(INST_JIT_PARAMS) {
@@ -124,11 +171,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -139,9 +181,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst + [imm64 & L3]
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x7c,         // i64.add
@@ -191,11 +231,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -206,9 +241,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst - [imm64 & L3]
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x7e,         // i64.sub
@@ -258,11 +291,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -273,9 +301,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst * [imm64 & L3]
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x7e,         // i64.mul
@@ -312,11 +338,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -327,9 +348,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst * [imm64 & L3] >> 64
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x10, $MUL128HI, // call $mul128hi (unsigned)
@@ -366,11 +385,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -381,9 +395,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst * [imm64 & L3] >> 64
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x10, $IMUL128HI, // call $imul128hi (signed)
@@ -475,11 +487,6 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
 				0x20, R(src), // local.get $src
-				0x42,         // i64.const
-			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
-			WASM_U8_THUNK({
-				0x7c, // i64.add
 			});
 			SCRATCHPAD_LOAD_L1_L2(inst);
 			WASM_U8_THUNK({
@@ -490,9 +497,7 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			// dst = dst ^ [imm64 & L3]
 			WASM_U8_THUNK({
 				0x20, R(dst), // local.get $dest
-				0x42,         // i64.const
 			});
-			WASM_I64(IMM_SEXT64(inst->imm32)); // $imm64
 			SCRATCHPAD_LOAD_L3(inst);
 			WASM_U8_THUNK({
 				0x85,         // i64.xor
@@ -586,19 +591,29 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 	if (opcode < ceil_FSWAP_R) {
 		int dst = inst->dst % 8;
 
-		if (dst < 4) {
-			// F(dst)
-		} else {
-			// E(dst - 4)
-		}
+		// perform a swap using a i8x16.swizzle
+		// element 0 : 8, 9, 10, 11, 12, 13, 14, 15,
+		// element 1 : 0, 1,  2,  3,  4,  5,  6,  7
 
-		// TODO: impl
+		int wdst = dst < 4 ? F(dst) : E(dst - 4);
+
+		// (dst0, dst1) = (dst1, dst0)
+		WASM_U8_THUNK({
+			0x20, wdst,                                           // local.get $dest
+			0xfd, 0x0c, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, // v128.const 0x0b0a0908 0x0f0e0d0c 0x03020100 0x07060504
+			0x0f, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, //
+			0xfd, 0x0e,                                           // i8x16.swizzle
+			0x21, wdst,                                           // local.set $dest
+		});
+
+		THUNK_END;
 	}
 
 	if (opcode < ceil_FADD_R) {
 		int dst = inst->dst % 4;
 		int src = inst->src % 4;
 
+		// dst = dst + src
 		WASM_U8_THUNK({
 			0x20, F(dst), // local.get $dest
 			0x20, A(src), // local.get $src
@@ -606,6 +621,126 @@ uint32_t jit_vm_inst(INST_JIT_PARAMS) {
 			0x11, 3, 0,   // call_indirect table 0, type (v128, v128) -> v128
 			0x21, F(dst), // local.set $dest
 		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FADD_M) {
+		int dst = inst->dst % 4;
+		int src = inst->src % 4;
+
+		// dst = dst + [src + imm64 & mod.mem ? L1 : L2]
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x20, R(src), // local.get $src
+		});
+		SCRATCHPAD_LOAD_F_L1_L2(inst);
+		WASM_U8_THUNK({
+			0x23, $fprc,  // global.get $fprc
+			0x11, 3, 0,   // call_indirect table 0, type (v128, v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FSUB_R) {
+		int dst = inst->dst % 4;
+		int src = inst->src % 4;
+
+		// dst = dst - src
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x20, A(src), // local.get $src
+			0x23, $fprc,  // global.get $fprc
+			0x11, 3, 1,   // call_indirect table 1, type (v128, v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FSUB_M) {
+		int dst = inst->dst % 4;
+		int src = inst->src % 4;
+
+		// dst = dst - [src + imm64 & mod.mem ? L1 : L2]
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x20, R(src), // local.get $src
+		});
+		SCRATCHPAD_LOAD_F_L1_L2(inst);
+		WASM_U8_THUNK({
+			0x23, $fprc,  // global.get $fprc
+			0x11, 3, 1,   // call_indirect table 1, type (v128, v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FSCAL_R) {
+		int dst = inst->dst % 4;
+
+		// (dst0, dst1) = (dst0 ^ 0x80F0000000000000, dst0 ^ 0x80F0000000000000)
+		WASM_U8_THUNK({
+			0x20, F(dst),                                         // local.get $dest
+			0xfd, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, // v128.const 0x00000000 0x80f00000 0x00000000 0x80f00000
+			0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x80, //
+			0xfd, 0x51,                                           // v128.xor
+			0x21, F(dst),                                         // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FMUL_R) {
+		int dst = inst->dst % 4;
+		int src = inst->src % 4;
+
+		// dst = dst * src
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x20, A(src), // local.get $src
+			0x23, $fprc,  // global.get $fprc
+			0x11, 3, 2,   // call_indirect table 2, type (v128, v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FDIV_M) {
+		int dst = inst->dst % 4;
+		int src = inst->src % 4;
+
+		// dst = dst * [src + imm64 & mod.mem ? L1 : L2]
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x20, R(src), // local.get $src
+		});
+		SCRATCHPAD_LOAD_F_L1_L2(inst);
+		WASM_U8_THUNK({
+			0x23, $fprc,  // global.get $fprc
+			0x11, 3, 3,   // call_indirect table 3, type (v128, v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
+	}
+
+	if (opcode < ceil_FSQRT_R) {
+		int dst = inst->dst % 4;
+
+		// dst = sqrt(dst)
+		WASM_U8_THUNK({
+			0x20, F(dst), // local.get $dest
+			0x23, $fprc,  // global.get $fprc
+			0x11, 4, 4,   // call_indirect table 4, type (v128) -> v128
+			0x21, F(dst), // local.set $dest
+		});
+
+		THUNK_END;
 	}
 
 	THUNK_END;
